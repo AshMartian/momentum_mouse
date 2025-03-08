19e22831f08d280b48bae578f14936083845f020
Ash Martian <ash.lee.martian@gmail.com>
2025-03-08 14:03:57 -0800

    Improve lint script to store and check gcc return code

--- lint.sh
+++ lint.sh
@@ -5,7 +5,15 @@ FILE_TO_LINT=${1:-src/inertia_scroller.c}
 # Use gcc for static analysis instead of splint
 gcc -Wall -Wextra -Werror -pedantic -fsyntax-only -I./include "$FILE_TO_LINT"
 
+# Store the return code
+RESULT=$?
+
 # If gcc succeeds, print a success message
-if [ $? -eq 0 ]; then
+if [ $RESULT -eq 0 ]; then
     echo "No errors found in $FILE_TO_LINT"
+else
+    echo "Errors found in $FILE_TO_LINT"
 fi
+
+# Return the gcc exit code
+exit $RESULT
